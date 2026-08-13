d7f61bedbdff2556114789d797e09e7405a08aef
Sander Spiegelaar <101831129+SanderSpiegelaar@users.noreply.github.com>
2026-08-13 12:03:00 +0200

    fix scripts

--- compose/scripts/setup-postgres-es.sh
+++ compose/scripts/setup-postgres-es.sh
@@ -17,9 +17,9 @@ nc -z -w 10 ${POSTGRES_SEEDS} ${DB_PORT:-5432}
 echo 'PostgreSQL port is available'
 
 # Create and setup temporal database
-temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -p ${DB_PORT:-5432} --db temporal create
-temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -p ${DB_PORT:-5432} --db temporal setup-schema -v 0.0
-temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -p ${DB_PORT:-5432} --db temporal update-schema -d /etc/temporal/schema/postgresql/v12/temporal/versioned
+temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -pw ${POSTGRES_PASSWORD} -p ${DB_PORT:-5432} --db ${POSTGRES_DB} create
+temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -pw ${POSTGRES_PASSWORD} -p ${DB_PORT:-5432} --db ${POSTGRES_DB} setup-schema -v 0.0
+temporal-sql-tool --plugin postgres12 --ep ${POSTGRES_SEEDS} -u ${POSTGRES_USER} -pw ${POSTGRES_PASSWORD} -p ${DB_PORT:-5432} --db ${POSTGRES_DB} update-schema -d /etc/temporal/schema/postgresql/v12/temporal/versioned
 
 # Setup Elasticsearch index
 # temporal-elasticsearch-tool is available in v1.30+ server releases
